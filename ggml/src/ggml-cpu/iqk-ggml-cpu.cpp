ba6d80bf84fef25886cd2bac63253ff30d9f6dfc
YingkunZhou <zhouyingkun19@163.com>
2025-08-21 08:32:57 +0800

    fix avx512 iqk_num_rows bug

--- ggml/src/ggml-cpu/iqk-ggml-cpu.cpp
+++ ggml/src/ggml-cpu/iqk-ggml-cpu.cpp
@@ -3462,6 +3462,7 @@ static inline enum ggml_type iqk_is_dequant_better(enum ggml_type type, int nrc_
 static inline int iqk_num_rows(enum ggml_type type) {
 #ifdef HAVE_FANCY_SIMD
     switch (type) {
+        case GGML_TYPE_IQ2_KS_T: return QK_T;
         case GGML_TYPE_Q8_1:
         case GGML_TYPE_Q8_K_R8: return 8;
         case GGML_TYPE_Q8_0_R8: return 16;
